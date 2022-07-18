b5afcdc4aeb5f651d2898f1d969e6ee6ffaa9f1e
Stefan Leitner <stefan-leitner@student.tugraz.at>
2022-07-18 15:37:55 +0200

    add disjoint baseline
    improve some print statements

--- disc.py
+++ disc.py
@@ -1,57 +1,45 @@
 from dua import *
+from statistics import mean
+from os.path import exists
+from training import train
 
 
 def disc(args, net, severity, common_corruptions):
     ckpt = torch.load(args.ckpt_path)
     bn_file_name = 'BN-' + net.__class__.__name__ + '-' + args.dataset + '.pt'
     load_bn_stats_file(net, bn_file_name)
+    common_corruptions = ['initial'] + common_corruptions
 
+    print('::: DISC Plug & Play :::')
     for args.level in severity:
-        print(f'Starting DISC for Level {args.level}')
         all_errors = []
         for idx, args.corruption in enumerate(common_corruptions):
             task_errors = []
-            print(f'Corruption - {args.corruption} :::: Level - {args.level}')
             load_bn_stats(net, args.corruption, ckpt)
-            teloader = prepare_test_data(args)[1]
-            err_cls = test(teloader, net)[0] * 100
-            print(f'DISC: Error: {err_cls:.1f}')
+            test_loader = prepare_test_data(args)[1]
+            err_cls = test(test_loader, net)[0] * 100
+            print(f'Error on Task-{idx} ({args.corruption}): {err_cls:.1f}')
             all_errors.append(err_cls)
             task_errors.append(err_cls)
 
             # previously seen tasks
             if idx > 0:
-                print(f'\tPreviously seen')
+                print('\tPreviously seen tasks:')
                 for i in range(0, idx):
                     load_bn_stats(net, common_corruptions[i], ckpt)
                     args.corruption = common_corruptions[i]
-                    teloader = prepare_test_data(args)[1]
-                    prev_err = test(teloader, net)[0] * 100
-                    print(f'\tError for {common_corruptions[i]}: {prev_err:.1f}')
+                    test_loader = prepare_test_data(args)[1]
+                    prev_err = test(test_loader, net)[0] * 100
+                    print(f'\tError on Task-{i} ({common_corruptions[i]}): '
+                          f'{prev_err:.1f}')
                     task_errors.append(prev_err)
+
                     if i == idx - 1:
-                        print(f'\tCurrent and prev seen mean error: {sum(task_errors) / len(task_errors):.1f}')
+                        print(f'\tMean error over current task '
+                              f'({common_corruptions[idx]}) and previously '
+                              f'seen tasks: {mean(task_errors):.1f}')
 
-        print(f'DISC Mean Error {(sum(all_errors) / len(all_errors)):.1f}')
-
-
-def baseline_src_only(net, severity, corruptions, args, ckpt=None):
-    """
-        Evaluate Source-Only baseline.
-    """
-    print('::: Baseline Source-Only :::')
-    if ckpt is not None:
-        net.load_state_dict(ckpt)
-        net.eval()
-    for level in severity:
-        all_errors = []
-        for corruption in corruptions:
-            args.corruption = corruption
-            teloader = prepare_test_data(args)[1]
-            err_cls = test(teloader, net)[0] * 100
-            print(f'Corruption - {corruption} - Level {level}, Source-Only Error: {err_cls:.1f}')
-            all_errors.append(err_cls)
-        print(f'Level {level}, Source-Only Mean Error: {(sum(all_errors) / len(all_errors)):.1f}')
+            assert mean(task_errors) == mean(all_errors)
 
 
 def load_bn_stats(net, corruption, ckpt=None):
@@ -59,10 +47,14 @@ def load_bn_stats(net, corruption, ckpt=None):
         Optionally loads a model checkpoint.
         Loads the running estimates for all batch norm layers
             from net.bn_stats attribute, for a given corruption.
+            Unless the corruption is 'initial', in which case the function
+            returns without loading anything.
         Sets network to evaluation mode.
     """
     if ckpt is not None:
         net.load_state_dict(ckpt)
+    if corruption == 'initial':
+        return
     state_dict = net.state_dict()
     for layer_name, value in net.bn_stats[corruption].items():
         state_dict[layer_name + '.running_mean'] = value['running_mean']
@@ -76,3 +68,64 @@ def load_bn_stats_file(net, file_path):
         Loads a saved state of net.bn_stats attribute, located at file_path.
     """
     net.bn_stats = torch.load(file_path)
+
+
+def baseline_source_only(net, severity, corruptions, args):
+    """
+        Evaluate Source-Only baseline.
+    """
+    net.load_state_dict(torch.load(args.ckpt_path))
+    net.eval()
+    scenario = 'provided checkpoint'
+
+    corruptions = ['initial'] + corruptions
+
+    print(f'::: Baseline Source-Only ({scenario}) :::')
+    for level in severity:
+        print(f'Corruption level of severity: {level}')
+        all_errors = []
+        for idx, args.corruption in enumerate(corruptions):
+            test_loader = prepare_test_data(args)[1]
+            err_cls = test(test_loader, net)[0] * 100
+            all_errors.append(err_cls)
+            print(f'Error on Task-{idx} ({args.corruption}): {err_cls:.1f}')
+            print(f'Mean error over current task ({args.corruption}) '
+                  f'and previously seen tasks: {mean(all_errors):.2f}')
+
+
+def baseline_disjoint(net, severity, corruptions, args, scenario='online'):
+    """
+        Evaluate Disjoint baseline.
+    """
+    if scenario == 'online':
+        args.epochs = 1
+    elif scenario == 'offline':
+        args.epochs = 150
+    args.lr = 0.01
+    ckpt_folder = 'checkpoints/disjoint/' + scenario + '/'
+    ckpt_folder += net.__class__.__name__ + '/'
+    corruptions = ['initial'] + corruptions
+
+    print(f'::: Baseline Disjoint ({scenario}) :::')
+    for level in severity:
+        print(f'Corruption level of severity: {level}')
+        all_errors = []
+        net.load_state_dict(torch.load(args.ckpt_path))
+        for idx, args.corruption in enumerate(corruptions):
+            if args.corruption != 'initial':
+                ckpt_path = ckpt_folder + args.corruption + '.pt'
+                if not exists(ckpt_path):
+                    print(f'No checkpoint for disjoint Task-{idx} '
+                          f'({args.corruption}) - Starting training.')
+                    net.load_state_dict(torch.load(args.ckpt_path))
+                    train(net, args, results_path=ckpt_folder)
+                else:
+                    net.load_state_dict(torch.load(ckpt_path))
+            net.eval()
+            test_loader = prepare_test_data(args)[1]
+            err_cls = test(test_loader, net)[0] * 100
+            all_errors.append(err_cls)
+            print(f'Error on Task-{idx} ({args.corruption}): {err_cls:.1f}')
+            print(f'Mean error over current task ({args.corruption}) '
+                  f'and previously seen tasks: {mean(all_errors):.2f}')
+
